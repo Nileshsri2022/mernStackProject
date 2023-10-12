23e3d31a348a3d1c31e174b0d8db1526fddd5432
Nilesh Srivastava <nilesh.srivastava.51273@gmail.com>
2023-10-13 00:30:24 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Home } from "./pages/Home";
+import {About} from "./pages/About"
+import {SignOut} from "./pages/SignOut"
+import {Signup} from "./pages/Signup"
+import {Profile} from "./pages/Profile"
+import  {BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./pages/Header";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header/>
+   
+    <Routes>
+      <Route path="/"element={<Home/>}/>
+      <Route path="/about"element={<About/>}/>
+      <Route path="/sign-up"element={<Signup/>}/>
+      <Route path="/sign-out"element={<SignOut/>}/>
+      <Route path="/profile"element={<Profile/>}/>
+
+    </Routes>
+    </BrowserRouter>
   );
 }
 
