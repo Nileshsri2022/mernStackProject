08b058a8bea156014a68f93af2181b38e34ce264
Nilesh Srivastava <Nileshsri2022>
2023-10-13 15:46:42 +0530

    3rd commit

--- api/index.js
+++ api/index.js
@@ -1,6 +1,29 @@
 import  express  from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
+const app=express();
+//1st way
+// const MONGO_URL = process.env.URL;
+// const connectdb = async (MONGO_URL)=>{
+//     try{
+        
+//         await mongoose.connect(MONGO_URL)
+//         console.log("db connected")
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+// }
+// connectdb(MONGO_URL);
+// 2nd way
+mongoose.connect(process.env.URL)
+.then(()=>{
+    console.log("connected")
+})
+.catch(err=>console.log(err))
 
-const app=express()
+
 
 app.listen(3000,()=>
 {
